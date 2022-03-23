139f179115bad3984997e5b1535dfbd9ad60314d
Utkarsh Sharma <utkkarsh@users.noreply.github.com>
2022-03-24 02:38:10 +0530

    Update RuleSet.route.js

--- Routes/RuleSet.route.js
+++ Routes/RuleSet.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const RuleSet = require("../Controllers/RuleSet.controller");
+const RuleSet = require("../Controllers/Ruleset.controller.js");
 
 router.post("/", RuleSet.create);
 router.get("/", RuleSet.get);
